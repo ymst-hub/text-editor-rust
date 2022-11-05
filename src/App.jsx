59a253ddf8212a893ce88b0a58c26c2e70508b60
ymst-hub <64212002+ymst-hub@users.noreply.github.com>
2022-11-06 00:41:18 +0900

    メソッド呼び出しを理解しました

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
 
 function App() {
+  const [greetMsg, setGreetMsg] = useState("");
   const [text, setText] = useState("");
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { text }));
+  }
   //todo ボタンの処理を追加して、保存できるようにする（ファイルがあれば追記）
   //todo ファイル検索を作成する
   return (
     <div>
-      <button>
-        こんにちは
+      <button type="button" onClick={() => greet()}>
+        Greet
       </button>
+      <p>{greetMsg}</p>
       <textarea id="texts" name="texts"
         rows="16" cols="33"
         onChange={(e) => setText(e.currentTarget.value)}
