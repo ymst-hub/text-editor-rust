86d3cb28a7cc75cce4f2c568904c2afff3d18a0b
ymst-hub <64212002+ymst-hub@users.noreply.github.com>
2022-11-06 21:28:35 +0900

    テキストエディタとして完成（リファクタ前）

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,16 +2,25 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use std::io::prelude::*;
+use std::{fs::File, io::Write,io::BufReader};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(text: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", text)
+fn save(path: &str, text: &str){
+    let mut file = File::create(path).unwrap();
+    file.write_all(text.as_bytes()).unwrap();
+    println!("save");
+}
+
+#[tauri::command]
+fn open(path: &str) -> String{
+    std::fs::read_to_string(&path).unwrap()
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![save,open])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import { open, save } from '@tauri-apps/api/dialog'
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
   const [text, setText] = useState("");
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { text }));
+  //ボタンの処理
+  async function save_txt() {
+    let path = await save({
+      multiple: false,
+      filters: [{
+        name: 'title',
+        extensions: ['txt']
+      }]
+    })
+    if (path == null || path == undefined){ return }
+    await invoke("save", { path, text })
+  }
+  async function open_txt() {
+    let path = await open({
+      multiple: false,
+      filters: [{
+        name: 'title',
+        extensions: ['txt']
+      }]
+    })
+    if (path == null || path == undefined) { return }
+    setText(await invoke("open", { path }))
+    
   }
   //todo ボタンの処理を追加して、保存できるようにする（ファイルがあれば追記）
   //todo ファイル検索を作成する
   return (
     <div>
-      <button type="button" onClick={() => greet()}>
-        Greet
+      <button type="button" onClick={() => save_txt()}>
+        Save
       </button>
-      <p>{greetMsg}</p>
+
+      <button type="button" onClick={() => open_txt()}>
+        Open
+      </button>
+
       <textarea id="texts" name="texts"
+        value={text}
         rows="16" cols="33"
         onChange={(e) => setText(e.currentTarget.value)}
       >
